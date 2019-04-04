9eeb4211e6aab9d3b7b56bed83802345cc5aaa08
Pratik Anurag <38415712+pratikanurag@users.noreply.github.com>
2019-04-04 15:47:42 +0530

    updated admin.py to add download csv functionality

--- slack_app/slack_appl/admin.py
+++ slack_app/slack_appl/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import ThirdPartyTransferError
+class ThirdPartyTransferErrorAdmin(admin.ModelAdmin):
+    actions = ['download_csv']
+    list_display = ("tenant_name","status" ,"dealer_id", "create_date", "machine_id")
+    def download_csv(self, request, queryset):
+        import csv
+        f = open('ro_failure.csv', 'w')
+        writer = csv.writer(f)
+        writer.writerow(["tenant_name","status" ,"dealer_id", "create_date", "machine_id"])
+        for s in queryset:
+            writer.writerow([s.tenant_name, s.status, s.dealer_id, s.create_date, s.machine_id])
+    download_csv.short_description = "Download CSV file for selected failures."
 
-admin.site.register(ThirdPartyTransferError)
+admin.site.register(ThirdPartyTransferError,ThirdPartyTransferErrorAdmin)
